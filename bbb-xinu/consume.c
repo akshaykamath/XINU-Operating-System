8bf262142b0be51526ff8da03a24418aeedc0882
Akshay Kamath <akkamath@iu.edu>
2015-09-20 16:33:07 -0400

    removed consume produce source files from main directory

--- bbb-xinu/consume.c
+++
@@ -1,8 +0,0 @@
-#include <prodcons.h>
-
- void consumer(int count)
- {
-     //Code to consume values of global variable 'n' until the value
-     of n is less than or equal to count
-     //print consumed value e.g. consumed : 8
- }
