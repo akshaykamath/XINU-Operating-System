2b913bc31d1647378fa5d9491aa1a1750ff014a9
Sameedha <sbairagi@iu.edu>
2015-10-07 19:49:12 -0400

    Modified xsh_prodcons.c

--- bbb-xinu/shell/xsh_prodcons.c
+++ bbb-xinu/shell/xsh_prodcons.c
@@ -56,10 +56,16 @@ shellcmd xsh_prodcons(int nargs, char *args[])
 		return 1;
 	}
 	
-      future * f1 ;
-      f1 = future_alloc(FUTURE_EXCLUSIVE);	
-      resume( create(future_cons, 1024, 20, "cons1", 1, f1) );
-      resume( create(future_prod, 1024, 20, "prod1", 1, f1) );
+      future * f1, *f2, *f3 ;
+      f1 = future_alloc(FUTURE_EXCLUSIVE);
+      f2 = future_alloc(FUTURE_EXCLUSIVE);
+      f3 = future_alloc(FUTURE_EXCLUSIVE);	
+      resume( create(future_cons, 1024, 20, "fcons1", 1, f1) );
+      resume( create(future_prod, 1024, 20, "fprod1", 1, f1) );
+      resume( create(future_cons, 1024, 20, "fcons1", 1, f2) );
+      resume( create(future_prod, 1024, 20, "fprod1", 1, f2) );
+      resume( create(future_cons, 1024, 20, "fcons1", 1, f3) );
+      resume( create(future_prod, 1024, 20, "fprod1", 1, f3) );
 	
       return 0;
 }
