31ee39733576c133eb7a456b3178cb2afedcafd0
Akshay Kamath <akkamath@iu.edu>
2015-10-07 18:15:39 -0400

    Updating future_get.c

--- bbb-xinu/system/future_get.c
+++ bbb-xinu/system/future_get.c
@@ -4,5 +4,27 @@
 
 syscall future_get(future* futureRef,int* valueRef){
 
-	return SYSERR;	
+	intmask mask;			/* Saved interrupt mask		*/
+	struct	procent *prptr;		/* Ptr to process' table entry	*/
+
+	mask = disable();
+	
+	if (futureRef->state == FUTURE_VALID) {
+		*valueRef = futureRef->value; 
+		restore(mask);
+		
+		return OK;
+	}
+
+	if (futureRef->state == FUTURE_EMPTY) {		/* If caller must block	*/
+		prptr = &proctab[currpid];
+		prptr->prstate = PR_WAIT;	/* Set state to waiting	*/		
+		futureRef->state = FUTURE_WAITING;
+		futureRef->pid = currpid;
+		resched();			/*   and reschedule	*/
+	}
+	
+	*valueRef = futureRef->value; 
+	restore(mask);
+	return OK;
 }
